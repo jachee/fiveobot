741a00f953b4f33e471db965d92dbb57548ab499
Jake Achée <jachee@gmail.com>
2017-09-23 16:57:53 -0500

    v0.2 - Add time/geolocation db, city shoutout
    Added the time/geolocation database in sqlite, changed the way UTC offset is handled, started calling it
    UTC instead of GMT, dynamically generating city lookup shout-outs based on timezone.

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# fiveobot
-Twitter bot in python.
+# fiveobot 0.2
+This is a twitter bot that, on the hour checks to see in which timezones it's 17:00. It then chooses a city and country from within those time zones and sends a shout out.
+
+The timezone and geo-location data is stored in the included sqlite database.
+
+Timezone data derived from https://timezonedb.com
+  CC By 3.0 https://creativecommons.org/licenses/by/3.0/
+
+Geographic Location data from https://data.opendatasoft.com/explore/dataset/geonames-all-cities-with-a-population-1000%40public/information
+  CC By 3.0 http://creativecommons.org/licenses/by/3.0/
--- fiveobot.py
+++ fiveobot.py
@@ -1,49 +1,69 @@
-"""
+'''
 fiveobot.py - FiveOBot on twitter. It's five o'clock somewhere, every hour
-or so.
-"""
+or so. 
 
-# from creds import *
-import datetime, time, tweepy, creds
+Timezone data from https://timezonedb.com 
+  CC By 3.0 https://creativecommons.org/licenses/by/3.0/
+
+Geographic data from https://data.opendatasoft.com/explore/dataset/geonames-all-cities-with-a-population-1000%40public/information
+  CC By 3.0 http://creativecommons.org/licenses/by/3.0/
+'''
+
+import calendar, datetime, time, tweepy, creds, sqlite3, random
 
 def check_time():
     now = datetime.datetime.utcnow()
     return now.minute == 0
 
 def tweet_out(phrase):
-    # print("I would tweet now, if I were able.")
     auth = tweepy.OAuthHandler(creds.consumer_key, creds.consumer_secret)
     auth.set_access_token(creds.access_token, creds.access_token_secret)
     api = tweepy.API(auth)
     print(phrase)
     api.update_status(phrase)
 
-
-def tz_phrase():
-    gmt = datetime.datetime.utcnow()
-    if gmt.hour >= 0 and gmt.hour <= 5:
-        offset = 17 - gmt.hour - 24
-        if offset >= 0:
-            offstr = ''.join(['+', str(offset)])
-        else:
-            offstr = str(offset)
+def tz_offset():
+    utc = datetime.datetime.utcnow()
+    if utc.hour >= 0 and utc.hour <= 5:
+        offset = 17 - utc.hour - 24
     else:
-        offset = 17 - gmt.hour
-        if offset >= 0:
-            offstr = ''.join(['+', str(offset)])
-        else:
-            offstr = str(offset)
+        offset = 17 - utc.hour
+    return offset * 3600
 
+def find_city(zones, cur):
+    zone = random.choice(zones)
+    query = 'SELECT "ASCII Name", "Country" FROM  geonames WHERE Timezone=?'
+    cityobj = cur.execute(query, (zone,))
+    cities = cityobj.fetchall()
+    return random.choice(cities)
 
-    return "It's five o'clock somewhere! Currently UTC %s" % offstr
+def find_zones(offset, cur):
+    ''' Finds the Region/City style name(s) that match a given timezone
+        offset from .  Expects an integer for seconds offset,
+        Returns a list of strings.'''
 
-def find_city():
-    pass
+    query = ('SELECT DISTINCT z.zone_name '
+             'FROM timezone tz JOIN zone z ON tz.zone_id=z.zone_id '
+             'WHERE tz.time_start <= strftime("%s", "now") '
+             'AND tz.gmt_offset=? ORDER BY tz.time_start')
+
+    zoneobj = cur.execute(query, (offset,))
+    zones = []
+    for row in zoneobj.fetchall():
+        zones.append(row[0])
+    return zones
 
 def main():
+    conn = sqlite3.connect('tzdb.sqlite3')
+    cur = conn.cursor()
     while True:
         if check_time():
-            phrase = tz_phrase()
+            phrase = "It's five o'clock somewhere! "
+            offset = tz_offset()
+            zones = find_zones(offset, cur)
+            city = find_city(zones, cur)
+            print(city)
+            phrase = phrase + "For example: %s, %s!" % city
             tweet_out(phrase)
             time.sleep(60)
 
