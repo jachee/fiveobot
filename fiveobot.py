118a48fba856540fdcf5e065b760821e497f2516
Jake Achée <jachee@gmail.com>
2017-09-26 12:52:21 -0500

    PEP 8 formatting.
    Surprisingly few changes made. Thanks anaconda!

--- fiveobot.py
+++ fiveobot.py
@@ -1,19 +1,26 @@
 '''
 fiveobot.py - FiveOBot on twitter. It's five o'clock somewhere, every hour
-or so. 
+or so.
 
-Timezone data from https://timezonedb.com 
+Timezone data from https://timezonedb.com
   CC By 3.0 https://creativecommons.org/licenses/by/3.0/
 
 Geographic data from https://data.opendatasoft.com/explore/dataset/geonames-all-cities-with-a-population-1000%40public/information
   CC By 3.0 http://creativecommons.org/licenses/by/3.0/
 '''
 
-import calendar, datetime, time, tweepy, creds, sqlite3, random
+import datetime
+import time
+import tweepy
+import creds
+import sqlite3
+import random
+
 
 def check_time():
     now = datetime.datetime.utcnow()
     return now.minute == 0
+
 
 def tweet_out(phrase):
     auth = tweepy.OAuthHandler(creds.consumer_key, creds.consumer_secret)
@@ -22,6 +29,7 @@ def tweet_out(phrase):
     print("Tweeting out: %s " % phrase)
     api.update_status(phrase)
 
+
 def tz_offset():
     utc = datetime.datetime.utcnow()
     if utc.hour >= 0 and utc.hour <= 5:
@@ -29,6 +37,7 @@ def tz_offset():
     else:
         offset = 17 - utc.hour
     return offset * 3600
+
 
 def find_city(zones, cur):
     zone = random.choice(zones)
@@ -39,6 +48,7 @@ def find_city(zones, cur):
     city = random.choice(cities)
     print("Selected City: ", city)
     return city
+
 
 def find_zones(offset, cur):
     ''' Finds the Region/City style name(s) that match a given timezone
@@ -57,6 +67,7 @@ def find_zones(offset, cur):
     print("Timezone List: ", zones)
     return zones
 
+
 def main():
     conn = sqlite3.connect('tzdb.sqlite3')
     cur = conn.cursor()
@@ -70,7 +81,6 @@ def main():
             phrase = phrase + "For example: %s, %s!" % city
             tweet_out(phrase)
         time.sleep(60)
-
 
 
 if __name__ == '__main__':
